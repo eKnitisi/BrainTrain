6342b8f252cf82e6f2ae4fcf38603e3052c89917
Tibe Simons <tibesimons@hotmail.be>
2025-04-10 16:54:32 +0200

    update

---
+++ .angular/cache/19.2.5/BrainTrain/vite/deps/primeng_config.js
@@ -0,0 +1,16 @@
+import {
+  PRIME_NG_CONFIG,
+  PrimeNG,
+  ThemeProvider,
+  providePrimeNG
+} from "./chunk-6CBJVMKW.js";
+import "./chunk-PPU34DSP.js";
+import "./chunk-53XSTCZ2.js";
+import "./chunk-NWEST3AH.js";
+import "./chunk-WDMUDEB6.js";
+export {
+  PRIME_NG_CONFIG,
+  PrimeNG,
+  ThemeProvider,
+  providePrimeNG
+};
